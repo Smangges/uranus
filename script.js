3d8bdf5c60381440803a242611307c51629199a7
Iqbal Mohammad Abdul Ghoni <iqballmags@gmail.com>
2017-01-14 17:14:16 +0700

    Initial Commit Base Design

---
+++ script.js
@@ -0,0 +1,11 @@
+$(function () {
+  AOS.init();
+
+  $(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') && ( $(e.target).attr('class') != 'dropdown-toggle' ) ) {
+      $(this).collapse('hide');
+    }
+  });
+
+  $('body').scrollspy({ target: '#site-navbar' });
+});
